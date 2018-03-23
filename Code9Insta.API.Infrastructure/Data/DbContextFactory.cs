c3513d3ed6bc790d9ca7a1c58b0d2347d9fed526
Mirjana Jovanovic <mirjana1211@gmail.com>
2018-03-23 23:00:07 +0100

    Added IDesignTimeDbContextFactory

--- Code9Insta.API.Infrastructure/Data/DbContextFactory.cs
+++ Code9Insta.API.Infrastructure/Data/DbContextFactory.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Code9Insta.API.Infrastructure.Data
 {
@@ -12,10 +13,11 @@ namespace Code9Insta.API.Infrastructure.Data
     {
         public CodeNineDbContext CreateDbContext(string[] args)
         {
+            
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=CodeNineDB;Trusted_Connection=True;";
             var builder = new DbContextOptionsBuilder<CodeNineDbContext>();
+            builder.UseSqlServer(connectionString);
 
-
-           // builder.UseSqlServer();
             return new CodeNineDbContext(builder.Options);
         }
     }
